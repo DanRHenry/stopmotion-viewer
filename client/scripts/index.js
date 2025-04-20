1a4c149d4945ba1917f88d907b8d50dec2564093
DanRHenry <danielrhenry@gmail.com>
2025-04-19 22:07:56 -0400

    basic functionality working

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,90 +1,123 @@
-const container = document.getElementById('container')
-const getPhotosListBtn = document.getElementById("getPhotosListBtn")
-const getPhotoBtn = document.getElementById("getPhotoBtn")
-const serverURL = "http://127.0.0.1:3521"
+const body = document.getElementById("body");
+const container = document.getElementById("container");
+const getPhotosListBtn = document.getElementById("getPhotosListBtn");
+const getPhotoBtn = document.getElementById("getPhotoBtn");
+const serverURL = "http://127.0.0.1:3521";
 
-// getPhotosBtn.addEventListener("click",fetchpictures)
-getPhotosListBtn.addEventListener("click",fetchPictureDirectoryListing)
-getPhotoBtn.addEventListener("click", getPicture)
+let directoryListing;
 
-let directoryListing
+async function fetchPictureDirectoryListing(subdir) {
+  let url;
+  if (subdir !== undefined) {
+    url = `${serverURL}/photos/directory_listing/${subdir}`;
+  } else {
+    url = `${serverURL}/photos/directory_listing/*`;
+  }
 
-const imgsrc = `${serverURL}/public/media/0157.jpg`
+  const res = await fetch(url);
+  const data = await res.json();
 
-const picture = document.createElement("img")
-picture.src = imgsrc
+  directoryListing = await data.directoryListing;
 
-container.append(picture)
+  return directoryListing;
+}
 
-async function fetchPictureDirectoryListing () {
-    const url = `${serverURL}/photos/directory_listing`
-    const res = await fetch (url)
-    const data = await res.json()
+const mainDirListing = await fetchPictureDirectoryListing();
 
-    directoryListing = await data.directoryListing;
+let picturesLisings = [];
+for (let i = 0; i < mainDirListing.length; i++) {
+  let subdirectory = {};
+  const listing = await fetchPictureDirectoryListing(mainDirListing[i]);
+  subdirectory[mainDirListing[i]] = listing;
+  picturesLisings.push(subdirectory);
+}
 
-    localStorage.setItem("photos", directoryListing)
+for (let i = 0; i < mainDirListing.length; i++) {
+  const imageHolder = document.createElement("img");
+  imageHolder.id = `imagedir_${mainDirListing[i]}`;
+  imageHolder.textContent = "image...";
+  imageHolder.className = "image";
 
-    let pics = []
-    for(let i = 0; i < directoryListing.length; i++) {
-        let res = await fetch(`${serverURL}/public/media/${directoryListing[i]}`)
-        console.log("res: ",res.url)
-        pics.push(res.url)
-    }
-let index = 0
-setInterval(() => {
-    picture.src = `${pics[index]}`
-    if (index++ < pics.length) {
-        index ++
+  const buttonHolder = document.createElement("div");
+  const slowBtn = document.createElement("input");
+  slowBtn.id = `slowBtn_${mainDirListing[i]}`;
+  slowBtn.type = "radio";
+  slowBtn.addEventListener("click", () => {
+    document.getElementById(`medBtn_${mainDirListing[i]}`).checked = false;
+
+    document.getElementById(`fastBtn_${mainDirListing[i]}`).checked = false;
+    speed = slow;
+  });
+
+  const medBtn = document.createElement("input");
+  medBtn.id = `medBtn_${mainDirListing[i]}`;
+  medBtn.type = "radio";
+  medBtn.checked = true;
+  medBtn.addEventListener("click", () => {
+    document.getElementById(`slowBtn_${mainDirListing[i]}`).checked = false;
+
+    document.getElementById(`fastBtn_${mainDirListing[i]}`).checked = false;
+    speed = medium;
+  });
+
+  const fastBtn = document.createElement("input");
+  fastBtn.id = `fastBtn_${mainDirListing[i]}`;
+  fastBtn.type = "radio";
+
+  fastBtn.addEventListener("click", () => {
+    document.getElementById(`slowBtn_${mainDirListing[i]}`).checked = false;
+
+    document.getElementById(`medBtn_${mainDirListing[i]}`).checked = false;
+
+    speed = fast;
+  });
+  const button = document.createElement("button");
+  button.innerText = "Start Stopmotion";
+
+  body.append(imageHolder);
+
+  buttonHolder.append(button);
+  imageHolder.after(buttonHolder);
+  button.before(slowBtn, medBtn, fastBtn);
+
+  let imgpath = picturesLisings[i][mainDirListing[i]];
+
+  let dirname = mainDirListing[i];
+
+  let stop = true;
+  let fast = 350;
+  let medium = 1000;
+  let slow = 1500;
+  let speed = medium;
+  button.addEventListener("click", () => {
+    if (stop === false) {
+      stop = true;
+      button.textContent = "Start Stopmotion"
     } else {
-        index = 0;
+      stop = false;
+      button.textContent = "Stop Stopmotion"
+      displayImage(speed);
     }
-}, 1000);
-}
+  });
 
+  let index = 0;
 
-async function getPicture(link) {
+  async function displayImage() {
+    if (stop === false) {
+      setTimeout(() => {
+        const pictureURL = `${serverURL}/public/media/${dirname}/${imgpath[index]}`;
 
-    if (!localStorage.photos) {
-        await fetchPictureDirectoryListing()
+        const imageSet = `imagedir_${dirname}`;
+
+        document.getElementById(imageSet).src = pictureURL;
+
+        if (index < imgpath.length - 1) {
+          index++;
+        } else {
+          index = 0;
+        }
+        displayImage(imgpath, dirname, speed);
+      }, speed);
     }
-    const pictureArray = localStorage.photos.split(",")
-    // console.log(pictureArray.length)
-
-    const path = `"${pictureArray[0]}"`
-
-    // console.log("path: ",path)
-    const url = `${serverURL}/photos/find_one`
-    // const url = `http://127.0.0.1:3521/photos/findphoto:${path}`
-        console.log("url: ",url)
-
-        const data = await fetch(url)
-        const imageSource = await data;
-
-        const blob = imageSource.blob()
-        const imageUrl = URL.createObjectURL(blob)
-        const imageElement = document.createElement("img")
-        imageElement.src = imageUrl
-        body.appendChild(imageElement)
-        console.log(imageSource)
-        
-        console.log("here")
-
+  }
 }
-// for (let i = 0; i < item.length; i++) {
-    // console.log(item)
-    // const blob = item.blob();
-    // const imageUrl = URL.createObjectURL(item)
-    // const imageElement = document.createElement("img")
-    // imageElement.src = imageUrl;
-    // container.appendChild(imageElement)
-// }
-
-// async function fetchImage() {
-//     const url = 'http://127.0.0.1:3521/images'; // Replace with your image URL
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-// }
-
--- server/controllers/photo.controller.js
+++ server/controllers/photo.controller.js
@@ -1,16 +1,25 @@
 const router = require("express").Router();
-// const fs = require("fs").promises;
-const fs = require("fs");
+const fs = require("fs").promises;
 
-const prefix = "http://127.0.0.1:3521"
-const filePath = "/server/public/media";
-const filespath = "../public/media"
-const fullpath = prefix + filePath
+const prefix = "http://127.0.0.1:3521";
+const filePath = "/home/dan/coding/apps/stopmotion-viewer/server/public/media";
 
-router.get("/directory_listing", async (req, res) => {
+let fullpath = filePath;
+
+router.get("/directory_listing/:subdir", async (req, res) => {
+  const { subdir } = req.params;
+  let info = subdir;
+
+  if (info === "*") {
+    info = "";
+  }
+
+  if (info !== undefined) {
+    fullpath = fullpath + info;
+  }
+
   try {
     const directoryListing = await getDirectoryListing(fullpath);
-
     directoryListing
       ? res.status(200).json({
           message: "Directory Listing:",
@@ -19,6 +28,9 @@ router.get("/directory_listing", async (req, res) => {
       : res.status(404).json({
           message: "No files found",
         });
+
+    fullpath = prefix + filePath + "/";
+    fullpath = filePath + "/";
   } catch (err) {
     console.error("server-side error");
     return res.status(500).json({
@@ -27,65 +39,61 @@ router.get("/directory_listing", async (req, res) => {
   }
 });
 
-// router.get("/find_one", async (req, res) => {
-//   try {
-//     console.log("trying...")
-//     const path = "/server/public/media/0157.jpg"
-//     // const {path} = req.params.path
-//     const picture = await getPicture(path)
+router.get("/find_one:link", async (req, res) => {
+  try {
+    console.log("trying...")
+    // const path = "/server/public/media/0157.jpg"
+    const {link} = req.params
+    console.log("link: ",link)
 
-//     picture
-//       ? res.status(200).json({
-//         message: "Picture:",
-//         picture
-//       })
-//       : res.status(404).json({
-//         message: "not found"
-//       })
-//     // console.log(req)
-//   } catch (err) {
-//     console.error("Server-side error");
-//     return res.status(500).json({
-//       Error: err.message,
-//     });
-//   }
-//   console.log("all tuckered out");
-// });
+    const picture = await getPicture(path)
 
+    picture
+      ? res.status(200).json({
+        message: "Picture:",
+        picture
+      })
+      : res.status(404).json({
+        message: "not found"
+      })
+  } catch (err) {
+    console.error("Server-side error");
+    return res.status(500).json({
+      Error: err.message,
+    });
+  }
+  console.log("all tuckered out");
+});
 
-// async function getPicture () {
-//   const path = "/server/public/media/0157.jpg"
-//   try {
-//     await fs.readFile(path, function (err, data) {
-//       if (err) {
-//         console.log("path error: ",path)
-//         console.error(err)
-//         return
-//       }
-//               console.log("path success: ",path)
+async function getPicture () {
+  const path = "/server/public/media/0157.jpg"
+  try {
+    await fs.readFile(path, function (err, data) {
+      if (err) {
+        console.log("path error: ",path)
+        console.error(err)
+        return
+      }
+              console.log("path success: ",path)
 
-//       console.log(data)
-//       return data
-//     }
-//   )} catch (err) {
-//     console.log("catch path",path)
-//     console.error(`Got an error: ${err.message}`)
-//   }
-// }
+      console.log(data)
+      return data
+    }
+  )} catch (err) {
+    console.log("catch path",path)
+    console.error(`Got an error: ${err.message}`)
+  }
+}
 
-async function getDirectoryListing() {
-  console.log("getting directory listing");
-const filePath = "/home/dan/coding/apps/stopmotion-viewer/server/public/media";
+async function getDirectoryListing(fullpath) {
   try {
     const directoryListing = [];
-    console.log(__dirname)
-    const data = await fs.promises.readdir(filePath);
+    const data = await fs.readdir(fullpath);
 
     for (let i = 0; i < data.length; i++) {
       const pictureLocation = data[i];
       directoryListing.push(`${pictureLocation}`);
     }
-    console.log("done getting directory listings...");
     return directoryListing;
   } catch (err) {
     console.error(`Got an error trying to read the directory: ${err.message}`);
